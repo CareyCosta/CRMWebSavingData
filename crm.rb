ba030e3b1756c403659a7074dde9154d819f06ff
Carey Costa <careyxcosta@gmail.com>
2017-01-25 15:55:13 -0500

    edit contact

--- crm.rb
+++ crm.rb
@@ -37,3 +37,12 @@ else
   raise Sinatra::NotFound
 end
 end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
